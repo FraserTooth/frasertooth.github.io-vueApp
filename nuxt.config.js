5bb99eb46a9dd77959b371695f6a6e0a8899ff1e
Fraser Tooth <frasertooth@gmail.com>
2019-11-14 13:30:46 +0900

    Added Google Analytics

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,7 +30,7 @@ export default {
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: [],
+  plugins: [{ src: '~plugins/ga.js', mode: 'client' }],
   /*
    ** Nuxt.js dev-modules
    */
