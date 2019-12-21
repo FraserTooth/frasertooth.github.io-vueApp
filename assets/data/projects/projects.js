f67d369e2e8fbf2aed811e4923ad5b5b9882336e
Fraser Tooth <frasertooth@gmail.com>
2019-12-21 21:17:56 +0900

    New Project Added

--- assets/data/projects/projects.js
+++ assets/data/projects/projects.js
@@ -3,8 +3,17 @@ import alwyn from './alwyn.json'
 import goexamine from './goexamine.json'
 import reflectra from './reflectra.json'
 import fruit from './fruit.json'
+import thisWebsite from './thisWebsite.json'
 import streamingWhere from './streamingWhere.json'
 
-const projects = [qlab, alwyn, goexamine, reflectra, fruit, streamingWhere]
+const projects = [
+  qlab,
+  alwyn,
+  goexamine,
+  reflectra,
+  fruit,
+  thisWebsite,
+  streamingWhere
+]
 
 export default projects
