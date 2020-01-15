8db706713f26c9d228976ce439a028bff3d7214e
Fraser Tooth <frasertooth@gmail.com>
2020-01-15 14:34:43 +0900

    CC Added

--- assets/data/experience/experience.js
+++ assets/data/experience/experience.js
@@ -1,6 +1,7 @@
 import kiwipower from './kiwipower.json'
 import peterbrett from './peterbrett.json'
+import codechrysalis from './codechrysalis.json'
 
-const experience = [kiwipower, peterbrett]
+const experience = [codechrysalis, kiwipower, peterbrett]
 
 export default experience
